1f3a0c7db42737eda833861a64f536d25ff1d185
Bgorelik47 <bg10382n@pace.edu>
2021-02-26 21:45:17 +0000

    Finished 3 tasks

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -4,17 +4,34 @@
 
 int main () {
 
+   puts("Type some text (then ENTER):");
   /* Zero out the array */
   int letter_frequency[26] = {0};
-  int len, i;
   char buf[MAX_BUF];
+  int i = 0, len = 0;
 
-  do {
-  } while (len > 1);
-
+  printf("Enter your string "); 
+  fgets(buf, MAX_BUF, stdin);
+  len = strlen(buf) - 1;
+  
+  while (buf[i] != '\0') {
+   
+      if (buf[i] >= 'a' && buf[i] <= 'z') {
+        buf[i] = buf[i] - 32;
+         len = buf[i] - 'A';
+         letter_frequency[len]++;
+      }
+      else if(buf[i] >= 'A' && buf[i] <= 'Z'){
+         len = buf[i] - 'A';
+         letter_frequency[len]++;
+      }
+      i++;
+  }
+  
   printf("Distribution of letters in corpus:\n");
   for (i = 0; i < 26; i++) {
     // 'A' is ASCII code 65
     printf("%c: %d\n", 65+i, letter_frequency[i]);
   }
+      return 0;
 }
