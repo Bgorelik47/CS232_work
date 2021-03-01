d60dd39aee210f0c70f5aebd34fffee43393e899
Bgorelik47 <bg10382n@pace.edu>
2021-03-01 00:56:45 +0000

    Changed task 6 to pass sanity

--- assign4/task6/construct_3_strs.c
+++ assign4/task6/construct_3_strs.c
@@ -9,20 +9,35 @@ node_t * construct_3_strs() {
    struct node* x;
    struct node* y;
    struct node* z;
-    
-  x = malloc(sizeof(node_t));
-  y = malloc(sizeof(node_t));
-  z = malloc(sizeof(node_t));
-    
-  x->value = "CS232";
-  y->value = "is";
-  z->value = "awesome";
 
-  z->next = x;
-  y->next = z;
-  x->next = y;
+    x = malloc(sizeof(node_t));
+    y = malloc(sizeof(node_t));
+    z = malloc(sizeof(node_t));
     
-  return x;//just to pass compiler, please edit as needed.
+    x->value = (char*)malloc(sizeof(char)* 6);
+    y->value = (char*)malloc(sizeof(char)* 3);
+    z->value = (char*)malloc(sizeof(char)* 8);
+
+    for(int i = 0; i < 6; i++){
+      x->value[i] = "CS232\0"[i];
+    }
+
+    for(int i = 0; i < 3; i++){
+      y->value[i] = "is\0"[i];
+    }
+ 
+    for(int i = 0; i < 8; i++){
+      z->value[i] = "awesome\0"[i];
+    }
+
+    //Dereference each pointer to access the .next field in its pointee, 
+    //and use pointer assignment to set the .next field to point to the appropriate Node. 
+      x->next = y;
+      y->next = z;
+      z->next = x;
+      
+    return x;
+    //just to pass compiler, please edit as needed.
 }
 
 //You can ignore the following code for testing
@@ -30,7 +45,6 @@ int dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * x = construct_3_strs();
     return dump_all(x);
-
 }
 
 int dump_all(node_t * x) {
@@ -40,12 +54,16 @@ int dump_all(node_t * x) {
     node_t * z = y->next;
     printf(" %s\n", z->value);
     if(z->next != x) {
-    	printf("failed");
-	return -1;
+        printf("failed");
+    return -1;
     } else {
-        free(z);
-        free(y);
-        free(x);
+      free(z->value);
+      free(y->value);
+      free(x->value);
+      free(z);
+      free(y);
+      free(x); 
         return 0;
+
     }
 }
